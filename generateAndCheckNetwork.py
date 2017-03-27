e79e91f58774481cc70f4f9b29923345fe499eb1
WonkyKong <m_dewhirst@outlook.com>
2017-03-28 00:25:26 +0100

    Added line class.

--- generateAndCheckNetwork.py
+++ generateAndCheckNetwork.py
@@ -10,13 +10,39 @@ class Grid:
         self.x = str (gridStruct.x).split (',')
         self.y = str (gridStruct.y).split (',')
 
-    def to_xy (indicesStr):
+    def to_xy (self, indicesStr):
         indices = str (indicesStr).split (',')
-        return '{},{}'.format (self.x[indices[0]], self.y[indices[1]])
+        return '{},{}'.format (self.x[int (indices[0])-1].strip (),
+                               self.y[int (indices[1])-1].strip ())
 
-    def to_xy_array (indicesStr):
+    def to_xy_array (self, indicesStr):
         indices = str (indicesStr).split (',')
         return [self.x[indices[0]], self.y[indices[1]]]
+
+
+class Line:
+
+    def __init__ (self, lineStruct, grid):
+        self.startPosition  = grid.to_xy (str (lineStruct.start).strip ())
+        self.endPosition    = grid.to_xy (str (lineStruct.stop).strip ())
+        try:
+            self.lineStyle = '[linewidth={}]'.format (lineStruct.line_width.strip ())
+        except AttributeError:
+            try:
+                self.lineStyle = '[{}]'.format (lineStruct.line_style.strip ())
+            except AttributeError:
+                self.lineStyle = ''
+        if (hasattr (lineStruct, 'square_ends')):
+            self.lineEnds = ''
+        else:
+            self.lineEnds = '{c-c}'
+
+    def getTexString (self):
+        return '\\psline{}{}({})({})\n'.format (self.lineStyle,
+                                                  self.lineEnds,
+                                                  self.startPosition,
+                                                  self.endPosition)
+
 
 
 def createTexFile (fileName, xmlStruct):
@@ -34,15 +60,23 @@ def createTexFile (fileName, xmlStruct):
         scaleBox = 1
 
     # Populate the lines
+    lines = []
+    for xmlLine in xmlStruct.lines.line:
+        lines.append (Line (xmlLine, grid))
 
     # Populate the nodes
 
     # Write to the tex file
     with open (fileName, 'w') as texFile:
         texFile.write ('\\scalebox{{{}}}{{\\begin{{pspicture}}({})\n'.format (scaleBox, str (xmlStruct.picture_size).strip ()))
+
         # Add lines
+        for line in lines:
+            texFile.write (line.getTexString ())
+
         # Add nodes
-        texFile.write ('\\end{{pspicture}}}}\n')
+
+        texFile.write ('\\end{pspicture}}\n')
 
 
 if __name__ == "__main__":
