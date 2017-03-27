bf3a724e0fe2c53536fdc4719baf33fac649a337
WonkyKong <m_dewhirst@outlook.com>
2017-03-27 23:16:03 +0100

    Reads xml file.

--- generateAndCheckNetwork.py
+++ generateAndCheckNetwork.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+import sys
+from lxml import objectify
+
 
 if __name__ == "__main__":
-    pass
+
+    # Check that have at least one argument
+    if (len (sys.argv) < 2):
+        print "Must supply the name of an XML file."
+        sys.exit (-1)
+
+
+    # Get XML file
+    try:
+        with open (sys.argv[1], 'r') as xmlFile:
+            xmlString = xmlFile.read ().replace ('\n', '')
+    except NameError:
+        print "Unable to read the XML file."
+        sys.exit (-1)
+    xmlStruct = objectify.fromstring (xmlString)
+
+
+    # Generate tex file
