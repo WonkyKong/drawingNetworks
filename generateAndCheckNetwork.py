58b0ebe2075f34d10b4358e4e216dc01611e7958
WonkyKong <m_dewhirst@outlook.com>
2017-03-27 23:35:07 +0100

    Starting to write th tex file.

--- generateAndCheckNetwork.py
+++ generateAndCheckNetwork.py
@@ -4,13 +4,37 @@ import sys
 from lxml import objectify
 
 
+def createTexFile (fileName, xmlStruct):
+
+    # This is based on the npspic class in the original Matlab code
+
+
+    # Construct the grid
+
+    # Determine the scaling
+    try:
+        scaleBox = xmlStruct.scaleBox
+    except AttributeError:
+        scaleBox = 1
+
+    # Populate the lines
+
+    # Populate the nodes
+
+    # Write to the tex file
+    with open (fileName, 'w') as texFile:
+        texFile.write ('\\scalebox{{{}}}{{\\begin{{pspicture}}({})\n'.format (scaleBox, str (xmlStruct.picture_size).strip ()))
+        # Add lines
+        # Add nodes
+        texFile.write ('\\end{{pspicture}}}}\n')
+
+
 if __name__ == "__main__":
 
     # Check that have at least one argument
     if (len (sys.argv) < 2):
         print "Must supply the name of an XML file."
         sys.exit (-1)
-
 
     # Get XML file
     try:
@@ -21,5 +45,5 @@ if __name__ == "__main__":
         sys.exit (-1)
     xmlStruct = objectify.fromstring (xmlString)
 
-
     # Generate tex file
+    createTexFile ("tempName.tex", xmlStruct)
