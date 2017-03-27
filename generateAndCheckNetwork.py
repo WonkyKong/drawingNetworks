bbd35453f0f1d0352752e0ec0c7c1fe0e55fda35
WonkyKong <m_dewhirst@outlook.com>
2017-03-27 23:54:51 +0100

    Added grid class.

--- generateAndCheckNetwork.py
+++ generateAndCheckNetwork.py
@@ -4,12 +4,28 @@ import sys
 from lxml import objectify
 
 
+class Grid:
+
+    def __init__ (self, gridStruct):
+        self.x = str (gridStruct.x).split (',')
+        self.y = str (gridStruct.y).split (',')
+
+    def to_xy (indicesStr):
+        indices = str (indicesStr).split (',')
+        return '{},{}'.format (self.x[indices[0]], self.y[indices[1]])
+
+    def to_xy_array (indicesStr):
+        indices = str (indicesStr).split (',')
+        return [self.x[indices[0]], self.y[indices[1]]]
+
+
 def createTexFile (fileName, xmlStruct):
 
     # This is based on the npspic class in the original Matlab code
 
 
     # Construct the grid
+    grid = Grid (xmlStruct.grid)
 
     # Determine the scaling
     try:
