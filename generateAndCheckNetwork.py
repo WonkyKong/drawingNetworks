bbed06c33b909e100fb36b40f998d3e46e73bddf
WonkyKong <m_dewhirst@outlook.com>
2017-03-28 16:22:29 +0100

    Added Node class and started to add Multiplier class.

--- generateAndCheckNetwork.py
+++ generateAndCheckNetwork.py
@@ -12,12 +12,99 @@ class Grid:
 
     def to_xy (self, indicesStr):
         indices = str (indicesStr).split (',')
-        return '{},{}'.format (self.x[int (indices[0])-1].strip (),
-                               self.y[int (indices[1])-1].strip ())
+        return '{},{}'.format (self.x[int (indices[0]) - 1].strip (),
+                               self.y[int (indices[1]) - 1].strip ())
 
     def to_xy_array (self, indicesStr):
         indices = str (indicesStr).split (',')
-        return [self.x[indices[0]], self.y[indices[1]]]
+        return [float (self.x[int (indices[0]) - 1]),
+                float (self.y[int (indices[1]) - 1])]
+
+
+class Node (object):
+
+    def __init__ (self, nodeStruct, grid):
+        self.position = grid.to_xy_array (nodeStruct.position)
+        self.ARROW_LENGTH   = 0.09
+        self.ARROW_NUDGE    = 0.02
+
+    def __leftArrow (self, shapeLimits):
+        ex = self.position[0] + shapeLimits[0] + self.ARROW_NUDGE
+        sx = ex - self.ARROW_LENGTH
+        ey = self.position[1]
+        sy = ey
+        return [ex, sx, ey, sy]
+
+    def __bottomArrow (self, shapeLimits):
+        ex = self.position [0]
+        sx = ex
+        ey = self.position [1] + shapeLimits [1] + self.ARROW_NUDGE
+        sy = ey - self.ARROW_LENGTH
+        return [ex, sx, ey, sy]
+
+    def __rightArrow (self, shapeLimits):
+        ex = self.position[0] + shapeLimits[2] - self.ARROW_NUDGE
+        sx = ex + self.ARROW_LENGTH
+        ey = self.position[1]
+        sy = ey
+        return [ex, sx, ey, sy]
+
+    def __topArrow (self, shapeLimits):
+        ex = self.position [0]
+        sx = ex
+        ey = self.position [1] + shapeLimits [3] - self.ARROW_NUDGE
+        sy = ey + self.ARROW_LENGTH
+        return [ex, sx, ey, sy]
+
+    def addArrows (self, shapeLimits, arrows):
+        returnString = ''
+        for arrow in arrows:
+            [ex,sx,ey,sy] = {
+                'l': self.__leftArrow   (shapeLimits),
+                'b': self.__bottomArrow (shapeLimits),
+                'r': self.__rightArrow  (shapeLimits),
+                't': self.__topArrow    (shapeLimits),
+            }[str (arrow).strip ()]
+            returnString += '\\psline[{}]({},{})({},{})\n'.format ('linecolor=white,linewidth=2pt', sx, sy, ex, ey)
+            returnString += '\\psline[{}]({},{})({},{})\n'.format ('arrowinset=0,arrows=->', sx, sy, ex, ey)
+        return returnString
+
+    def getTexString (self):
+        return ''
+
+
+class Multiplier (Node):
+
+    def __init__ (self, nodeStruct, grid):
+        Node.__init__ (self, nodeStruct, grid)
+        self.TRI_HEIGHT     = 0.5
+        self.TRI_LENGTH     = 0.6
+        self.TEXT_HEIGHT    = 0.45
+        self.direction      = nodeStruct.direction
+        self.coefficient    = nodeStruct.coefficient
+        self.triangle       = [ [-self.TRI_LENGTH / 2,   self.TRI_HEIGHT / 2],
+                                [-self.TRI_LENGTH / 2,  -self.TRI_HEIGHT / 2],
+                                [ self.TRI_LENGTH / 2,   0]]
+
+    def getTexString (self):
+        texString = ''
+
+        # Draw the arrows
+        texString += super (Multiplier, self).addArrows ([self.TRI_LENGTH * 0.5 * x for x in [-1, -1, 1, 1]], self.direction)
+
+        # Draw the triangle
+
+        # Set the lengths according to the orientation
+
+        # Add the coefficient
+
+        return texString
+
+
+def NodeFactory (nodeStruct, grid):
+    type = str (nodeStruct.type).strip ()
+    if type == 'multiplier': return Multiplier (nodeStruct, grid)
+    return Node (nodeStruct, grid)
 
 
 class Line:
@@ -65,16 +152,21 @@ def createTexFile (fileName, xmlStruct):
         lines.append (Line (xmlLine, grid))
 
     # Populate the nodes
+    nodes = []
+    for xmlNode in xmlStruct.nodes.node:
+        nodes.append (NodeFactory (xmlNode, grid))
 
     # Write to the tex file
     with open (fileName, 'w') as texFile:
         texFile.write ('\\scalebox{{{}}}{{\\begin{{pspicture}}({})\n'.format (scaleBox, str (xmlStruct.picture_size).strip ()))
 
-        # Add lines
+        # Add lines to the tex file
         for line in lines:
             texFile.write (line.getTexString ())
 
-        # Add nodes
+        # Add nodes to the tex file
+        for node in nodes:
+            texFile.write (node.getTexString ())
 
         texFile.write ('\\end{pspicture}}\n')
 
